33b3467f928001a0bdfc3382ac7012f41aab6ce2
Group 3 <126671047+team3seas@users.noreply.github.com>
2023-03-13 10:02:32 +0100

    Add files via upload
    LAB 3 Excercise - Temperature AND Humidity sensor

---
+++ Musaab Hisham Hamed Ahmed/LAB3_T_AND_H.ino
@@ -0,0 +1,42 @@
+#include "DHT.h"
+#define DHTTYPE DHT11
+
+#define dht_dpin D1
+
+DHT dht(dht_dpin, DHTTYPE);
+
+void setup()
+{
+  dht.begin();
+  Serial.begin(9600);
+  delay(100);
+  Serial.println("Initializing ESP8255...");
+}
+
+void loop()
+{
+  delay(3000);
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  Serial.print("Current Humdity = ");
+  Serial.print(h);
+  Serial.print("% ");
+  Serial.print("Tempaerature = ");
+  Serial.print(t);
+  Serial.println("C ");
+if (t > 27 && h > 60) {
+  Serial.println("AC + FAN");
+}
+else if (t < 18 && h < 40) {
+  Serial.println("HEATER");
+}
+else if (t > 27 && h < 40) {
+  Serial.println("AC");
+}
+else if (t < 18 && h > 60) {
+  Serial.println("Heater + Fan");
+}
+else {
+  Serial.println("Conditions are optimal");
+}
+}
